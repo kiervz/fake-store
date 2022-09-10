8d796ed96874a3faac9cb722a958bb32ec58092d
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-09-11 00:48:59 +0800

    fix products card

--- src/pages/products/ProductItems.js
+++ src/pages/products/ProductItems.js
@@ -1,21 +1,31 @@
 import React from 'react'
+import { FaShoppingCart, FaHeart, FaExpand } from 'react-icons/fa'
 import './ProductItems.css'
 
 const ProductItems = ({product}) => {
-  return (
-    <div className='card'>
-        <div className='card__image'>
-            <img src={product.image} alt={product.title}/>
-        </div>
-        <div className='card__body'>
-            <div className='card__title'>
-                { product.title.substring(0,30) }
-                { product.title.length > 30 ? '...' : '' }
+    return (
+        <div className="col-md-6 col-lg-6 col-xl-4">
+            <div className="single__product">
+                <div className="image__part" style={
+                        {
+                            background: `url(${product.image}) no-repeat center`,
+                            backgroundSize: 'cover'
+                        }
+                    }>
+                    <ul>
+                        <li><div><FaShoppingCart /></div></li>
+                        <li><div><FaHeart /></div></li>
+                        <li><div><FaExpand /></div></li>
+                    </ul>
+                </div>
+                <div className="text__part">
+                    <h3 className="product__title">{product.title}</h3>
+                    <h4 className="product__old__price">$79.99</h4>
+                    <h4 className="product__price">${product.price}</h4>
+                </div>
             </div>
-            <div className='card__price'>{ product.price }</div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ProductItems
--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -11,7 +11,7 @@ const Products = () => {
     const [products, setProducts] = useState([]) 
 
     const getProducts = () => {
-        axios.get('https://fakestoreapi.com/products?limit=10') 
+        axios.get('https://fakestoreapi.com/products') 
             .then(({data}) => {
                 setProducts(data)
             }).catch((error) => console.log(error.response))
@@ -22,14 +22,14 @@ const Products = () => {
     }, [])
 
     return (
-        <div>
+        <div className='products'>
             <Navbar />
-            <div className='row container-fluid'>
+            <div className='row container-fluid mt-20'>
                 <div className='col-md-3'>
                     <CategoryList />
                 </div>
                 <div className='col-md-9'>
-                    <div className='products'>
+                    <div className='row'>
                         { products.map(product => <ProductItems product={product} key={product.id} />) }
                     </div>
                 </div>
