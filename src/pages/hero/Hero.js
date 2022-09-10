f0b3fa6b4eb588ab02a21ae85fbe3fb8405822f6
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-09-10 10:25:58 +0800

    separate hero page

---
+++ src/pages/hero/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <section className='hero'>
+        <h4>NEW INSPIRATION 2022</h4>
+        <h2>Super value deals</h2>
+        <h2 className='text-success'>On all products</h2>
+        <p>Save more with coupons & up to 70% off!</p>
+
+        <Link to="/products" className="shop__now">
+            <span className="circle" aria-hidden="true">
+                <span className="icon arrow"></span>
+            </span>
+            <span className="button__text">Shop Now</span>
+        </Link>
+    </section>
+  )
+}
+
+export default Hero
